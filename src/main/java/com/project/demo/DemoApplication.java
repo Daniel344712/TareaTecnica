19df8d7f4ae63e9cb779489a93f94f3522788c52
Daniel344712 <156951237+Daniel344712@users.noreply.github.com>
2025-10-05 13:47:56 -0600

    estructura del backend

---
+++ src/main/java/com/project/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//Patron decorador, metodo para darle caracteristicas a una clase
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
